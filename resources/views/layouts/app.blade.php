8a4328df68fb45aec05300f4371a86e3030dc85c
frangersal <frangersal@outlook.com>
2022-11-18 17:38:06 -0600

    Fix: layout nav top, log out and re size of elemen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,32 +35,84 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img class="nav-logo" src="{{ asset('img/logo/FGStore_logo.png') }}"  alt="icono">
+                    <img class="nav-logo-tiny" src="{{ asset('img/logo/FGStore_logo_tiny.png') }}"  alt="icono">
                 </a> 
-                    <div class="input-group mb-3 cqlch nav-search-section">
-                        <input type="text" class="nav-serch-bar" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <button class="btn nav-serch-bar-buttton" type="button" id="button-addon2">
-                            <img class="nav-serch-bar-icon" src="{{ asset('img/icons/icons8-search-50.png') }}"  alt="icono">
-                        </button>
-                    </div> 
 
-                <button class="nav-button-perfil nav-button " >
-                    <img class="nav-button-avatar" src="{{ asset('img/testpersonas/el_negro.jpg') }}"  alt="icono">
-                    <span class="nav-button-text">
-                        Perfil
-                    </span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <button class="nav-button-perfil nav-button " >
-                    <img class="nav-button-avatar" src="{{ asset('img/icons/icons8-favorite-50.png') }}"  alt="icono">
-                    <span class="nav-button-text">
-                        Wishlist
-                    </span>
-                </button>
-                <button class="nav-button-perfil nav-button " >
-                    <img class="nav-button-avatar" src="{{ asset('img/icons/icons8-shopping-cart-30.png') }}"  alt="icono">
-                    <span class="nav-button-text">
-                        Carrito
-                    </span>
-                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+ 
+                    <ul class="navbar-nav ms-auto">
+                         
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item nav-button-perfil nav-button">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item nav-button-perfil nav-button">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                        
+                            <div class="input-group mb-3 cqlch nav-search-section">
+                                <input type="text" class="nav-serch-bar" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn nav-serch-bar-buttton" type="button" id="button-addon2">
+                                    <img class="nav-serch-bar-icon" src="{{ asset('img/icons/icons8-search-50.png') }}"  alt="icono">
+                                </button>
+                            </div> 
+                            <div class=" cqlch ">
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle nav-button-perfil nav-button" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <img class="nav-button-avatar" src="{{ asset('img/testpersonas/el_negro.jpg') }}"  alt="icono">    
+                                        <span class="nav-button-text">
+                                            {{ Auth::user()->name }}
+                                        </span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            </div>
+                            
+                            <div class="cqlch">
+                                <button class="nav-button-perfil nav-button " >
+                                    <img class="nav-button-avatar" src="{{ asset('img/icons/icons8-favorite-50.png') }}"  alt="icono">
+                                    <span class="nav-button-text">
+                                        Wishlist
+                                    </span>
+                                </button>
+                            </div>
+                            <div class="cqlch">
+                                <button class="nav-button-perfil nav-button " >
+                                    <img class="nav-button-avatar" src="{{ asset('img/icons/icons8-shopping-cart-30.png') }}"  alt="icono">
+                                    <span class="nav-button-text">
+                                        Carrito
+                                    </span>
+                                </button>
+                            </div>
+                        @endguest
+                    </ul>
+                </div>
+                <!-- -->
 <!-- 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
